4d5dc24a0a6815167ab351c4b59834176bc3fd4f
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2025-10-03 10:43:31 -0300

    ..

---
+++ projeto/src/components/ContactItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function ContatoList({ contatos, removerContato, abrirEdicao }) {
+  return (
+    <div className="contacts">
+      {contatos.map((contato) => (
+        <div key={contato.id} className="contact-item">
+          <div>
+            <div className="contato-nome">{contato.nome}</div>
+            <div className="contato-numero">{contato.telefone}</div>
+          </div>
+
+          <div className="contact-actions">
+            {/* botão enviar mensagem */}
+            <button type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" stroke="#00b859" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send-icon">
+                <path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/>
+                <path d="m21.854 2.147-10.94 10.939"/>
+              </svg>
+            </button>
+
+            {/* botão editar */}
+            <button type="button" onClick={() => abrirEdicao(contato)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" stroke="#00b859" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-square-pen-icon">
+                <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/>
+              </svg>
+            </button>
+
+            {/* botão excluir */}
+            <button className="button-excluir" onClick={() => removerContato(contato.id)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" stroke="#b80000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash2-icon">
+                <path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
